e2cd5290d1bc9538fc2e82c5fc593241baf301c2
thewangcj <thewangcj@gmail.com>
2017-08-21 15:49:15 +0800

    finish favorite module

--- src/script/controller/favoriteCtrl.js
+++ src/script/controller/favoriteCtrl.js
@@ -1,4 +1,6 @@
 'use strict';
 angular.module('app').controller('favoriteCtrl',['$http','$scope',function($http,$scope){ 
-
+  $http.get('data/myFavorite.json').then(function(res){
+    $scope.list = res.data;
+  })
 }]);
--- src/script/directive/positionList.js
+++ src/script/directive/positionList.js
@@ -1,5 +1,5 @@
 'use strict';
-angular.module('app').directive('appPositionList',[function(){
+angular.module('app').directive('appPositionList',['$http','cache',function($http,cache){
   return{
     restrict: 'A',
     replace: true,
@@ -7,6 +7,14 @@ angular.module('app').directive('appPositionList',[function(){
     scope: {
       data: '=',
       filterObj: '='
+    },
+    link:function($scope){
+      $scope.name = cache.get('name') || '';
+      $scope.select =function(item){
+        $http.post('data/favorite.json',{id:item.id,select:!item.select}).then(function(){
+          item.select = !item.select;
+        });
+      }
     }
   }
 }]);
