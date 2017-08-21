730772aa5401c41d3904f21b8c4b10a1033db66a
thewangcj <thewangcj@gmail.com>
2017-08-21 15:15:17 +0800

    finsish login

--- src/script/controller/loginCtrl.js
+++ src/script/controller/loginCtrl.js
@@ -1,4 +1,11 @@
 'use strict';
-angular.module('app').controller('loginCtrl',['$http','$scope',function($http,$scope){ 
-  
+angular.module('app').controller('loginCtrl',['$http','$scope','$state','cache',function($http,$scope,$state,cache){ 
+  $scope.submit = function(){
+    $http.post('data/login.json',$scope.user).then(function(res){
+      cache.put('id',res.data.id);
+      cache.put('name',res.data.name);
+      cache.put('image',res.data.image);
+      $state.go('main');
+    });
+  }
 }]);
