a3d7465c9fcf8c5da10b9861a3b0db84a0c38ef5
thewangcj <thewangcj@gmail.com>
2017-08-21 13:32:45 +0800

    finish register module

--- src/script/controller/registerCtrl.js
+++ src/script/controller/registerCtrl.js
@@ -1,6 +1,26 @@
 'use strict';
-angular.module('app').controller('registerCtrl',['$http','$scope',function($http,$scope){ 
+angular.module('app').controller('registerCtrl',['$http','$scope','$interval',function($http,$scope,$interval){ 
   $scope.submit = function(){
     console.log($scope.user);
-  }
+  };
+
+  var count = 60;
+  $scope.send = function(){
+    $http.get('data/code.json').then(function(res){
+      if(1===res.data.state){
+        //倒计时
+        $scope.time = '60s';
+        var interval = $interval(function(){
+          if(count<=0){
+            $interval.cancel(interval); //取消计时
+            $scope.time = '';
+            return;
+          }else{
+            count--;
+            $scope.time = count + 's';
+          }
+        },1000)
+      }
+    });
+  };
 }]);
